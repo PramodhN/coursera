8085e2d02170b923ef0459320c32ea09191fe3c1
Pramodh Natarajan <pramodhn91@gmail.com>
2016-01-01 17:50:30 -0700

    angular factory and controllers

---
+++ front-end-javascript-frameworks/app/scripts/controllers.js
@@ -0,0 +1,96 @@
+'use strict';
+
+angular.module('confusionApp', [])
+
+        .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+
+            $scope.tab = 1;
+            $scope.filtText = '';
+            $scope.showDetails = false;
+
+            $scope.dishes= menuFactory.getDishes();
+
+            $scope.select = function(setTab) {
+                $scope.tab = setTab;
+
+                if (setTab === 2) {
+                    $scope.filtText = "appetizer";
+                }
+                else if (setTab === 3) {
+                    $scope.filtText = "mains";
+                }
+                else if (setTab === 4) {
+                    $scope.filtText = "dessert";
+                }
+                else {
+                    $scope.filtText = "";
+                }
+            };
+
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);
+            };
+
+            $scope.toggleDetails = function() {
+                $scope.showDetails = !$scope.showDetails;
+            };
+        }])
+
+        .controller('ContactController', ['$scope', function($scope) {
+
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+
+            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+
+            $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+
+        }])
+
+        .controller('FeedbackController', ['$scope', function($scope) {
+
+            $scope.sendFeedback = function() {
+
+                console.log($scope.feedback);
+
+                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+        }])
+
+        .controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+
+            $scope.dish= menuFactory.getDish(3);
+
+        }])
+
+        .controller('DishCommentController', ['$scope', function($scope) {
+
+
+            $scope.formdata = {rating:5};
+            $scope.submitComment = function () {
+
+                console.log($scope.formdata);
+
+                var date = new Date().toISOString();
+
+                $scope.formdata.date = date;
+
+                $scope.dish.comments.push($scope.formdata);
+
+                $scope.commentForm.$setPristine();
+
+                $scope.formdata = {rating:5};
+            }
+        }])
+
+;
